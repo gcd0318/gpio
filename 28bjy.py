939095e1593be645304cb50e8bbbff0ec011ef60
GUO, Chen <gcd0318@hotmail.com>
2014-02-17 03:20:07 +0800

    28byj works

--- 28bjy.py
+++ 28bjy.py
@@ -2,18 +2,39 @@ from RPi import GPIO
 import time
 from GCStatic import eb21
 
-def run(mode):
+def run(mode, step=4, idle=0):
     if(mode == 'board'):
         GPIO.setmode(GPIO.BOARD)
     elif(mode == 'bcm'):
         GPIO.setmode(GPIO.BCM)
     while(True):
-        eb21.setall((1,0,0,0))
-        eb21.setall((0,0,1,0))
-        eb21.setall((0,1,0,0))
-        eb21.setall((0,0,0,1))
+        if(4 == step):
+            eb21.setall((1,0,0,0))
+            time.sleep(idle)
+            eb21.setall((0,1,0,0))
+            time.sleep(idle)
+            eb21.setall((0,0,1,0))
+            time.sleep(idle)
+            eb21.setall((0,0,0,1))
+            time.sleep(idle)
+        elif(8==step):
+            eb21.setall((1,0,0,0))
+            time.sleep(idle)
+            eb21.setall((1,1,0,0))
+            time.sleep(idle)
+            eb21.setall((0,1,0,0))
+            time.sleep(idle)
+            eb21.setall((0,1,1,0))
+            time.sleep(idle)
+            eb21.setall((0,0,1,0))
+            time.sleep(idle)
+            eb21.setall((0,0,1,1))
+            time.sleep(idle)
+            eb21.setall((0,0,0,1))
+            time.sleep(idle)
+            eb21.setall((1,0,0,1))
+            time.sleep(idle)
 
 if __name__ == '__main__':
     GPIO.cleanup()
-    run('board')
-
+    run('board', 8, 0.01)
